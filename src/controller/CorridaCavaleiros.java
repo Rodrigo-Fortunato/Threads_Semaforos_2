d02f7b3c9b01fb1a648cd2f184b7726ed881eeb6
Rodrigo <rodrigo.neves@fatec.sp.gov.br>
2024-03-26 22:16:20 -0300

    Atualização do Exercicio

--- src/controller/CorridaCavaleiros.java
+++ src/controller/CorridaCavaleiros.java
@@ -1,14 +1,89 @@
 package controller;
 
+import java.util.Random;
+import java.util.concurrent.Semaphore;
+
 public class CorridaCavaleiros extends Thread {
 
-    public CorridaCavaleiros(){
-        super();
+    private Semaphore semaphoreTorch;
+    private Semaphore semaphoreShineStone;
+    private boolean torch= false;
+    private boolean shineStone;
+    private int id=0;
+
+    public CorridaCavaleiros(int id){
+        this.id = id;
     }
+
 
     @Override
     public void run() {
-        super.run();
+
+        cavaleiro();
+    }
+
+    private void cavaleiro(){
+        Random random = new Random();
+        int velocidadeMax=4;
+        int velocidadeMin=2;
+        int passos=0;
+        int distanciaPercorrida=0;
+        while (distanciaPercorrida < 2000){
+            passos =random.nextInt(velocidadeMin,velocidadeMax);
+            distanciaPercorrida += passos;
+            System.out.println("O cavaleiro andou: "+ passos + " e ja percorreu: "+distanciaPercorrida);
+
+
+            try {
+                sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (!torch && distanciaPercorrida >= 500){
+                velocidadeMin = setTorch(velocidadeMin);
+            }
+            if (!shineStone && distanciaPercorrida >= 1500){
+               velocidadeMin = setShineStone(velocidadeMin);
+            }
+
+        }
+        System.out.println("Cavaleiro "+ id + " chegou as portas");
+
+    }
+
+    private int setTorch(int velocidadeMin){
+
+
+            try {
+                semaphoreTorch.acquire();
+                if (!torch) {
+                    velocidadeMin = 4;
+                    torch = true;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }finally {
+                semaphoreTorch.release();
+            }
+
+        return velocidadeMin;
+    }
+
+    private int setShineStone(int velocidadeMin) {
+
+        try {
+            semaphoreShineStone.acquire();
+            if (!shineStone){
+                velocidadeMin =6;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            semaphoreShineStone.release();
+        }
+        return velocidadeMin;
+
     }
 
 
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,7 +1,13 @@
 package view;
 
+import controller.CorridaCavaleiros;
+
 public class Main {
     public static void main(String[] args) {
 
+        for (int cont =0;cont <4;cont++){
+            CorridaCavaleiros cavaleiros= new CorridaCavaleiros(cont);
+            cavaleiros.run();
+        }
     }
 }
